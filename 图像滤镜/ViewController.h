23fce8100caff72d6891c53e2cb830797de7aa9b
残月 <632342527@qq.com>
2016-11-16 18:45:18 +0800

    系统的图像滤镜使用
    系统的图像滤镜使用

--- 图像滤镜/ViewController.h
+++ 图像滤镜/ViewController.h
@@ -4,12 +4,30 @@
 //
 //  Created by 王奥东 on 16/11/10.
 //  Copyright © 2016年 王奥东. All rights reserved.
-//
+//  参考： 实战宝典 进阶篇
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+/**
+    要实现滤镜效果需要四个步骤:
+    1.存储图片数据所用的CIImage
+    2.对图片数据处理用的CIContext
+    3.图片的滤镜CIFilter
+    [CIFilter filterWithName:items[row] keysAndValues:kCIInputImageKey,ciImage, nil];
+    其中name为滤镜的名称，keysAndValues是滤镜的建和值
+    4.输出滤镜[filter outputImage]
+    而后将CGImage转换成UIImage使用
+ 
+ */
 
+
+@interface ViewController : UIViewController{
+    
+   IBOutlet UIImageView *imageView;
+    UIImage *orgImage;
+    NSArray *items;
+
+}
 
 @end
 
--- 图像滤镜/ViewController.m
+++ 图像滤镜/ViewController.m
@@ -8,7 +8,8 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+
+@interface ViewController ()<UIPickerViewDelegate,UIPickerViewDataSource>
 
 @end
 
@@ -16,14 +17,47 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    orgImage = [UIImage imageNamed:@"1.jpg"];
+    imageView.image = orgImage;
+    items = @[@"Original",@"CILinearToSRGBToneCurve",@"CIPhotoEffectChrome",@"CIPhotoEffectFade",@"CIPhotoEffectInstant",@"CIPhotoEffectMono",@"CIPhotoEffectNoir",@"CIPhotoEffectProcess",@"CIPhotoEffectTonal",@"CIPhotoEffectTransfer",@"CISRGBToneCurveToLinear",@"CIVignetteEffect"];
+}
+
+-(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView{
+    return 1;
+}
+
+-(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+    return items.count;
+}
+
+-(NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component {
+    return items[row];
+}
+
+-(void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component {
+
+    if (row == 0) {
+        imageView.image = orgImage;
+        return;
+    }
+    
+    //实例化图片与过滤器
+    CIImage *ciImage = [[CIImage alloc]initWithImage:orgImage];
+    CIFilter *filter = [CIFilter filterWithName:items[row] keysAndValues:kCIInputImageKey,ciImage, nil];
+
+    //设置过滤器中所有的输入值为默认值
+    [filter setDefaults];
+    CIContext *context = [CIContext contextWithOptions:nil];
+    //得到过滤后的图片
+    CIImage *outputImage = [filter outputImage];
+    CGImageRef cgImage = [context createCGImage:outputImage fromRect:[outputImage extent]];
+    imageView.image = [UIImage imageWithCGImage:cgImage];
+    CGImageRelease(cgImage);
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+
+
 
 
 @end
